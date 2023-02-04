4a7fb3d0c77ae4d149a55363b6d8deb3ebe10cf5
Filip Sufitchi <fsufitchi@gmail.com>
2023-02-04 00:48:37 +0000

    Add link icon decorations

--- app/components/markdown/dom/Anchor.vue
+++ app/components/markdown/dom/Anchor.vue
@@ -20,11 +20,34 @@ const $attachmentHref = computed(() =>
         ? $journal.value?.fileURL($articleID.value, $href.value)
         : '',
 );
+
+const $link: [string, string] = computed(() =>
+    $href.value.match('^[a-z]+://')
+        ? ['direct', $href.value]
+        : $href.value.startsWith('#')
+        ? ['anchor', $href.value]
+        : $href.value.startsWith('/')
+        ? ['route', $href.value]
+        : ['attachment', $journal.value?.fileURL($articleID.value, $href.value)],
+);
 </script>
 
 <template>
-    <a v-if="$directHref" :href="$directHref"> <slot /> </a>
-    <RouterLink v-else-if="$routerTo" :to="$routerTo"> <slot /> </RouterLink>
-    <a v-else-if="$attachmentHref" :href="$attachmentHref"> <slot /> </a>
-    <RouterLink v-else to="/page-not-found"> <slot /> </RouterLink>
+    <a v-if="$link[0] === 'direct' && $link[1]" :href="$link[1]">
+        <slot /><sup><VIcon icon="mdi-open-in-new" /></sup>
+    </a>
+
+    <a v-else-if="$link[0] === 'anchor' && $link[1]" :href="$link[1]">
+        <slot /><sup><VIcon icon="mdi-pound" /></sup>
+    </a>
+
+    <RouterLink v-else-if="$link[0] === 'route' && $link[1]" :to="$link[1]"> <slot /> </RouterLink>
+
+    <a v-else-if="$link[0] === 'attachment' && $link[1]" :href="$link[1]">
+        <slot /><sup><VIcon icon="mdi-cloud-download" /></sup>
+    </a>
+
+    <RouterLink v-else to="/page-not-found">
+        <slot /><sup><VIcon icon="mdi-link-variant-off" /></sup>
+    </RouterLink>
 </template>
