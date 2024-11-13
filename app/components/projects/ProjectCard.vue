389e55f9b0ac50cd31374dd1b6257bdcc2fcbaed
Filip Sufitchi <fsufitchi@gmail.com>
2024-11-12 19:20:48 -0500

    Refactor project display to be much prettier

---
+++ app/components/projects/ProjectCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps<{ image?: string; href?: string }>();
+</script>
+
+<template>
+    <VCard color="surface-variant">
+        <VCardItem>
+            <VCardTitle> <slot name="title" /> </VCardTitle>
+            <VCardSubtitle> <slot name="subtitle" /> </VCardSubtitle>
+            <template #append>
+                <VAvatar tile size="5em"><VImg :src="props.image" /></VAvatar>
+            </template>
+        </VCardItem>
+        <VCardText :class="$style['card-text']">
+            <slot name="body" />
+        </VCardText>
+        <VCardActions v-if="props.href">
+            <VSpacer />
+            <VBtn size="small" variant="outlined" color="primary" :href="props.href" target="_blank">
+                <VIcon icon="mdi-open-in-new" /> Check it out!
+            </VBtn>
+        </VCardActions>
+    </VCard>
+</template>
+
+<style lang="scss" module>
+.card-text {
+    & p {
+        margin-bottom: 0.75em;
+    }
+}
+</style>
--- app/components/projects/ProjectsMain.vue
+++ app/components/projects/ProjectsMain.vue
@@ -2,9 +2,13 @@
 import DiscordBoarBotImg from './discord-boar-bot.png';
 import ChocolateDoomImg from './chocolate-doom.png';
 import ShipSpinningImg from './ship-spinning.png';
+import WireguardImg from './wireguard.png';
+import PrezTweetingImg from './modern-day-presidential.png';
+import FaviconImg from './favicon.png';
 import { usePageMetadata } from '@app/page-metadata';
 
 import { GITHUB_URL } from '@app/constants';
+import ProjectCard from './ProjectCard.vue';
 
 usePageMetadata().value = {
     title: 'Projects - @fsufitch',
@@ -13,27 +17,41 @@ usePageMetadata().value = {
 </script>
 
 <template>
-    <VRow>
-        <VSpacer />
-        <VCol cols="12" lg="8" xl="6">
-            <VAlert>
-                <VIcon icon="mdi-github"></VIcon>
-                See more at my Github! <a :href="GITHUB_URL"> {{ GITHUB_URL }} </a>
-            </VAlert>
-        </VCol>
-        <VSpacer />
-    </VRow>
-
-    <VSpacer />
+    <VAlert class="ma-2" color="info">
+        <VIcon icon="mdi-github"></VIcon>
+        See more at my Github! <a :href="GITHUB_URL"> {{ GITHUB_URL }} </a>
+    </VAlert>
 
     <VRow>
-        <VSpacer />
-        <VCol cols="12" lg="8" xl="6">
-            <VCard>
-                <VImg class="d-none d-md-block" :src="DiscordBoarBotImg" width="256px" style="float: right" />
-                <VCardTitle> Discord Boar Bot </VCardTitle>
-                <VCardText>
-                    <VImg class="d-md-none mx-auto" :src="DiscordBoarBotImg" width="256px" />
+        <VCol cols="12" md="6" class="d-flex flex-column">
+            <ProjectCard class="ma-2" :image="FaviconImg" href="https://github.com/fsufitch/personal-landing-page">
+                <template #title>This Website!</template>
+                <template #subtitle> You're reading it </template>
+                <template #body>
+                    <p>
+                        This website you're viewing is both my greeting card to the world, and my testbed for all sorts
+                        of frontend experiments. It is a combination of a collection of mostly-static pages, plus a
+                        more-dynamic "journal" section.
+                    </p>
+                    <p>
+                        It is written using a very fun mix of tech. The code itself is Typescript/SCSS and uses the Vue
+                        framework. The site is styled using the Vuetify design framework.
+                    </p>
+                    <p>
+                        The journal (aka blog) section of the project is actually a renderer for my articles (aka blog
+                        posts) which are hosted in a
+                        <a href="https://github.com/fsufitch/journal" target="_blank"> separate Github repository</a>.
+                        The posts themselves are stored as YAML metadata and Markdown files. The journal web UI fetches
+                        this metadata and text and renders it nicely for the reader. It also adds support for special
+                        styling blocks in the Markdown documents (such as alerts, framed/captioned images, etc).
+                    </p>
+                </template>
+            </ProjectCard>
+
+            <ProjectCard class="ma-2" :image="DiscordBoarBotImg" href="https://github.com/fsufitch/discord-boar-bot">
+                <template #title>Discord Boar Bot</template>
+                <template #subtitle> Handy chatbot for all my friends</template>
+                <template #body>
                     <p>
                         <a href="https://discord.com" target="_blank">Discord<VIcon icon="mdi-open-in-new" /></a>
                         is a VoIP platform oriented towards providing text/voice communication to groups of video gamers
@@ -46,28 +64,52 @@ usePageMetadata().value = {
                         a Postgres database. It used to run in Heroku, but has since migrated to a small instance in
                         AWS.
                     </p>
+                </template>
+            </ProjectCard>
+
+            <ProjectCard class="ma-2" :image="ShipSpinningImg" href="https://github.com/fsufitch/prez-tweet">
+                <template #title>Ship Spinning Inc.</template>
+                <template #subtitle> Virtual slot machine for a virtual universe </template>
+                <template #body>
                     <p>
-                        <a href="https://github.com/fsufitch/discord-boar-bot" target="_blank">
-                            Check it out on Github! <VIcon icon="mdi-open-in-new" />
-                        </a>
+                        Ship Spinning Inc. was a web based slot machine game built around
+                        <a href="http://eveonline.com">Eve Online</a>, a massively-multiplayer online game. Ship
+                        Spinning connected with Eve via a complex XML API and created a system in which Eve Online
+                        players could use their in-game "credits" currency to activate a web-based animated slot
+                        machine, with which they could win in-game items prizes. The system integrated with other APIs,
+                        in order to measure and ensure good payouts and to better engage players. Over its lifetime, it
+                        provided entertainment for over 1000 players, and distributed prizes worth thousands of dollars
+                        (if converted from in-game currency).
                     </p>
-                </VCardText>
-            </VCard>
+                    <p>
+                        While it has no longer been maintained, its source code is still available online. Its user
+                        interface was a custom jQuery-based site. The server was built in Python, using the web2py
+                        framework and a SQLite database, and it was deployed on a virtual private server.
+                    </p>
+                </template>
+            </ProjectCard>
         </VCol>
-        <VSpacer />
-    </VRow>
+        <VCol cols="12" md="6" class="d-flex flex-column">
+            <ProjectCard class="ma-2" :image="WireguardImg" href="https://github.com/fsufitch/wg-vlan">
+                <template #title><code>wg-vlan</code></template>
+                <template #subtitle> Wireguard VLAN helper</template>
+                <template #body>
+                    <p>
+                        <a href="https://wireguard.com" target="_blank">Wireguard <VIcon icon="mdi-open-in-new" /></a>
+                        is a fast, simple, powerful VPN. Its configuration/management for can be a little complicated to
+                        get going smoothly... Or at least they were for me.
+                    </p>
+                    <p>
+                        Since my main use case is creating a VLAN organized around a central server, I made this tool
+                        for managing the requisite configurations.
+                    </p>
+                </template>
+            </ProjectCard>
 
-    <VSpacer />
-
-    <VRow>
-        <VSpacer />
-        <VCol cols="12" lg="8" xl="6">
-            <VCard>
-                <VImg class="d-none d-md-block" :src="ChocolateDoomImg" width="256px" style="float: right" />
-                <VCardTitle> Chocolate Doom </VCardTitle>
-                <VCardSubtitle> Minor contributions </VCardSubtitle>
-                <VCardText>
-                    <VImg class="d-md-none mx-auto" :src="ChocolateDoomImg" width="256px" />
+            <ProjectCard class="ma-2" :image="ChocolateDoomImg" href="https://github.com/chocolate-doom/chocolate-doom">
+                <template #title>Chocolate Doom</template>
+                <template #subtitle> (minor contributions) </template>
+                <template #body>
                     <p>
                         <a href="https://www.chocolate-doom.org" target="_blank">
                             Chocolate Doom <VIcon icon="mdi-open-in-new" />
@@ -98,22 +140,13 @@ usePageMetadata().value = {
                         </VCol>
                         <VSpacer />
                     </VRow>
-                </VCardText>
-            </VCard>
-        </VCol>
-        <VSpacer />
-    </VRow>
+                </template>
+            </ProjectCard>
 
-    <VSpacer />
-
-    <VRow>
-        <VSpacer />
-        <VCol cols="12" lg="8" xl="6">
-            <VCard>
-                <VImg class="d-none d-md-block" :src="ShipSpinningImg" width="256px" style="float: right" />
-                <VCardTitle> Ship Spinning Inc. </VCardTitle>
-                <VCardText>
-                    <VImg class="d-md-none mx-auto" :src="ShipSpinningImg" width="256px" />
+            <ProjectCard class="ma-2" :image="PrezTweetingImg" href="https://github.com/fsufitch/prez-tweet">
+                <template #title>Preztweeting.com</template>
+                <template #subtitle> A cry for help, maybe </template>
+                <template #body>
                     <p>
                         As a quick coding exercise to refresh myself on building webpages using Angular, I decided to
                         create a simple web UI allowing side-by-side comparison of two Twitter feeds — in this case, two
@@ -121,19 +154,14 @@ usePageMetadata().value = {
                         alternating which feed scrolls back to keep the displayed tweets as contemporaneous as possible.
                     </p>
                     <p>
-                        The user interface is coded using Angular and styled using Bootstrap. It is backed by a
-                        lightweight Go-based webserver that uses a Postgres database for indexing tweets, and runs
-                        regular crawls to keep the index up to date. It runs on Heroku and is deployed in a way that
-                        allows for seamless horizontal scaling in case of going viral.
+                        While it has no longer been maintained, its source code is still available online. The user
+                        interface is coded using Angular and styled using Bootstrap. It is backed by a lightweight
+                        Go-based webserver that uses a Postgres database for indexing tweets, and runs regular crawls to
+                        keep the index up to date. It runs on Heroku and is deployed in a way that allows for seamless
+                        horizontal scaling in case of going viral.
                     </p>
-                    <p>
-                        <a href="https://github.com/fsufitch/prez-tweet" target="_blank">
-                            Check it out on Github! <VIcon icon="mdi-open-in-new" />
-                        </a>
-                    </p>
-                </VCardText>
-            </VCard>
+                </template>
+            </ProjectCard>
         </VCol>
-        <VSpacer />
     </VRow>
 </template>
